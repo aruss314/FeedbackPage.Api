7e80abb2a34af687394580b0722697adb0fb18c1
Demonslyr <Demonslyr@users.noreply.github.com>
2021-07-21 21:06:01 -0500

    Swagger and healtcheck endpoints (#2)

---
+++ FeedbackPage.Api/Extensions/JsonSerializationOptions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace FeedbackPage.Api.Extensions
+{
+    public sealed class JsonSerializationOptions : IConfigureOptions<JsonOptions>
+    {
+        public void Configure(JsonOptions options)
+        {
+            options.JsonSerializerOptions.IgnoreNullValues = true;
+            options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+        }
+    }
+}
---
+++ FeedbackPage.Api/Swagger/OperationFilters/SecurityRequirementsOperationFilter.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FeedbackPage.Api.Swagger.OperationFilters
+{
+    public class SecurityRequirementsOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            // Policy names map to scopes
+            var requiredScopes = context.MethodInfo.DeclaringType.GetCustomAttributes(true) // Possible null ref Todo: Review to make this work a little more intented
+                .Union(context.MethodInfo.GetCustomAttributes(true))
+                .OfType<AuthorizeAttribute>()
+                .Select(attr => attr.Policy)
+                .Distinct();
+
+            if (!requiredScopes.Any())
+            {
+                return;
+            }
+
+            operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+            operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden" });
+
+            operation.Security = new List<OpenApiSecurityRequirement>
+            {
+                new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+
+                    }
+                }
+            };
+        }
+    }
+}
